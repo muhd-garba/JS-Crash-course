585732e78e84c4689b6b6e5d08834baa3ca0f34a
Muhammad Garba Muhd <muhdgarba1038@gmail.com>
2026-05-10 20:16:36 +0100

    add

--- main.js
+++ main.js
@@ -1,4 +1,38 @@
-// let const
-let score;
-score = 10;
-console.log(score);  
+// Simple JavaScript Todo List
+
+let todos = [];
+
+// Add a todo
+function addTodo(task) {
+    todos.push(task);
+    console.log(`Added: ${task}`);
+}
+
+// Remove a todo
+function removeTodo(index) {
+    if (index >= 0 && index < todos.length) {
+        console.log(`Removed: ${todos[index]}`);
+        todos.splice(index, 1);
+    } else {
+        console.log("Invalid index");
+    }
+}
+
+// Show all todos
+function showTodos() {
+    console.log("Todo List:");
+    
+    todos.forEach((todo, index) => {
+        console.log(`${index + 1}. ${todo}`);
+    });
+}
+
+// Example usage
+addTodo("Learn JavaScript");
+addTodo("Build a project");
+
+showTodos();
+
+removeTodo(0);
+
+showTodos();
